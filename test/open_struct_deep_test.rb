0497af844d294729a65150e7cd84871c9b6b48aa
Fred Kim <yhk1038@hanmail.net>
2018-04-18 00:12:21 +0900

    Add test: initialize case

--- test/open_struct_deep_test.rb
+++ test/open_struct_deep_test.rb
@@ -33,8 +33,24 @@ class OpenStructDeepTest < Minitest::Test
            ]
   }
   
-  def test_initialize_case
+  def test_it_still_run
     assert OpenStructDeep.new DATA
+  end
+  
+  def test_initialize_case
+    # case 1 : wrapping hash bracket
+    hash = {name: 'John Smith', age: 70, pension: 300}
+    assert OpenStructDeep.new hash
+    
+    # case 2 : inline
+    assert OpenStructDeep.new(name: 'John Smith', age: 70, pension: 300)
+    
+    # case 3 : append each data
+    person = OpenStructDeep.new
+    person.name    = "John Smith"
+    person.age     = 70
+    person.pension = 300
+    assert_equal 70, person.age
   end
   
   def test_it_makes_method_from_key
@@ -49,8 +65,10 @@ class OpenStructDeepTest < Minitest::Test
     
     # first_floor
     assert_equal 'Joseon', osd.country
+    
     # second_floor
     assert_equal 1392, osd.info.from
+    
     # third_floor
     assert_equal 'Lee Seong Gye', osd.info.started_by.name
   end
